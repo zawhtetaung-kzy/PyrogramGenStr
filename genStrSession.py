eab38142c713cc4c1a4de0ad1847334a457f8316
Shahsad Kolathur <shahsadkpklr@gmail.com>
2022-01-03 13:20:19 +0530

    Update genStrSession.py

--- genStrSession.py
+++ genStrSession.py
@@ -16,6 +16,7 @@ async def genStrSession() -> None:  # pylint: disable=missing-function-docstring
             device_model="CodeXBotz Ver 1.0.1"
     ) as codexbotz:
         print("\nprocessing...")
+        codexbotz.storage.SESSION_STRING_FORMAT=">B?256sQ?"
         await codexbotz.send_message(
             "me", f"<b>@CodeXBotz User Session</b>\n\n<code>{await codexbotz.export_session_string()}</code>\n\n<i>Keep this Safe and Not share anyone</i>")
         print("Done!, Check your Saved Message for String Session!")
