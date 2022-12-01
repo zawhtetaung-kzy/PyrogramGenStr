19ab7771f25fb64c78de351da8db3fa24befa35b
Shahsad KP <shahsadkpklr@gmail.com>
2022-12-01 19:47:59 +0530

    Update genStrSession.py

--- genStrSession.py
+++ genStrSession.py
@@ -2,17 +2,13 @@ import os
 import asyncio
 
 from pyrogram import Client
-from dotenv import load_dotenv
-
-if os.path.isfile("config.env"):
-    load_dotenv("config.env")
 
 
 async def genStrSession() -> None:  # pylint: disable=missing-function-docstring
     async with Client(
             "codexbotz",
-            api_id=int(os.environ.get("APP_ID") or input("Enter Telegram APP ID: ")),
-            api_hash=os.environ.get("API_HASH") or input("Enter Telegram API HASH: "),
+            api_id=int(input("Enter Telegram APP ID: ")),
+            api_hash=input("Enter Telegram API HASH: "),
             device_model="CodeXBotz Ver 1.0.1"
     ) as codexbotz:
         print("\nprocessing...")
