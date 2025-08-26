3720119a3d9af4d1f3a11a6fbb89879e9c6c915b
Zaw Htet Aung <zawhtetaung.coms@gmail.com>
2025-08-26 17:11:09 +0630

    Update genStrSession.py

--- genStrSession.py
+++ genStrSession.py
@@ -9,12 +9,12 @@ async def genStrSession() -> None:  # pylint: disable=missing-function-docstring
             "codexbotz",
             api_id=int(input("Enter Telegram APP ID: ")),
             api_hash=input("Enter Telegram API HASH: "),
-            device_model="CodeXBotz Ver 1.0.1"
+            device_model="TKSFamily Ver 1.0.1"
     ) as codexbotz:
         print("\nprocessing...")
         codexbotz.storage.SESSION_STRING_FORMAT=">B?256sQ?"
         await codexbotz.send_message(
-            "me", f"<b>@CodeXBotz User Session</b>\n\n<code>{await codexbotz.export_session_string()}</code>\n\n<i>Keep this Safe and Not share anyone</i>")
+            "me", f"<b>@TKSFamily User Session</b>\n\n<code>{await codexbotz.export_session_string()}</code>\n\n<i>Keep this Safe and Not share anyone</i>")
         print("Done!, Check your Saved Message for String Session!")
 
 if __name__ == "__main__":
